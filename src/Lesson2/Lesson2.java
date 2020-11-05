c1ff07891d9d9bbe957fcf096463b14e7c959e07
Ренат <hayrenat@gmail.com>
2020-11-05 17:26:23 +0300

    Домашнее задание 3 выполнено

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -5,7 +5,7 @@ import java.io.Reader;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class main {
+public class Lesson2 {
     public static void main(String[] args) {
 //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
